20ee9c74ca6d0803d72db6bd1df607b6bbfb94cc
asimboss32 <nmnuralom541@gmail.com>
2025-06-20 13:11:55 -0600

    authsettup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\dashboardController;
 use App\Http\Controllers\frontendController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route:: get('/',[frontendController::class,'index']);
+Route::get('/',[frontendController::class,'index']);
+
+//admin login
+Route::get('/admin/login',[AdminController::class,'adminlogin']);
+Auth::routes();
+
+Route::get('/admin/dashboard',[dashboardController::class,'admindashboard']);
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
